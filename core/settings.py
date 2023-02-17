d6049176ef94d27728b06559ddf84a2e8cf73fb2
ZahraAbiri <zahraabiri816@gmail.com>
2023-02-17 11:34:24 +0330

    add postgress config

--- core/settings.py
+++ core/settings.py
@@ -79,11 +79,30 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'shop',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '123456',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
